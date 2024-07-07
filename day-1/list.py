a2b4a979c1c46d8fa8b7b6e0314bcc20c6cc5c13
uppar0507 <uppar0507@gmail.com>
2024-07-07 23:20:10 +0530

    added few lines for sort and index

--- day-1/list.py
+++ day-1/list.py
@@ -36,6 +36,35 @@ stream.remove('electrical') #here i used remove function to remove the element c
 stream.pop('civil')   #here i used pop function to remove the element called "civil" both remove and pop do the same work.
 print(stream)
 
+#reverse
+stream = ['computer_science', 'civil', 'electronics', 'electrical', 'mechanical'] #here this is my list
+stream.reverse()  #here i used reverse function to reverse the list.
+print(stream)
 
+#sorting with ascending order and desceding order
+num = [1, 7, 9, 2, 4, 3]  #here this is my list of num
+num.sort  #here i'm using sort function
+print(num)
+
+num = [1, 7, 9, 2, 4, 3] #here this my list num
+num.sort(reverse=True) #here i'm calling list in reverse order
+print(num)
+
+stream = ['computer_science', 'civil', 'electronics', 'electrical', 'mechanical'] #here this is my list.
+sorted = sorted(stream) #here we are sorting the elements without modifying the original elements.
+print(stream)
+
+#getting min and max number using python.
+num =  [1, 7, 9, 2, 4, 3] #here is my list.
+print(min(num)) #here i'm getting minmum number from the list.
+print(max(num)) #here i'm getting maximum number from the list.
+print(sum(num)) #here i'm getting sum of the given list.
+
+#indexing the list.
+stream = ['computer_science', 'civil', 'electronics', 'electrical', 'mechanical'] #here is my list.
+print(stream.index('civil')) #here we are calling the index from given list.
+
+stream = ['computer_science', 'civil', 'electronics', 'electrical', 'mechanical'] #here is my list.
+print('bio_tech' in stream) #here i'm checking the wheather bio_tech is present or not from given list.
 
 
